ebf1ab320f5d7456e930a9180c17373c841a0134
Joash Mathew <joashmathew816@gmail.com>
2026-04-22 07:37:11 -0400

    Fix Firebase initializeApp in service worker and base.html

--- static/firebase-messaging-sw.js
+++ static/firebase-messaging-sw.js
@@ -1,25 +1,26 @@
-importScripts("https://www.gstatic.com/firebasejs/10.7.0/firebase-app-compat.js");
-importScripts("https://www.gstatic.com/firebasejs/10.7.0/firebase-messaging-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/10.12.0/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/10.12.0/firebase-messaging-compat.js");
 
-firebase.initializeApp({
-  apiKey: "AIzaSyCWRfI4zUk27QGPbO6crBwIuRr5xpoS0xk",
-  authDomain: "church-app-21147.firebaseapp.com",
-  projectId: "church-app-21147",
-  storageBucket: "church-app-21147.firebasestorage.app",
-  messagingSenderId: "681140747186",
-  appId: "1:681140747186:web:c5c0957b553e2361e902ab"
+const app = firebase.initializeApp({
+    apiKey: "AIzaSyCWRfI4zUk27QGPbO6crBwIuRr5xpoS0xk",
+    authDomain: "church-app-21147.firebaseapp.com",
+    projectId: "church-app-21147",
+    storageBucket: "church-app-21147.firebasestorage.app",
+    messagingSenderId: "681140747186",
+    appId: "1:681140747186:web:c5c0957b553e2361e902ab"
 });
 
-const messaging = firebase.messaging();
+const messaging = firebase.messaging(app);
 
 messaging.onBackgroundMessage(function(payload) {
-    console.log("Background message received:", payload);
+    console.log("[firebase-messaging-sw.js] Background message:", payload);
     self.registration.showNotification(
         payload.notification.title,
         {
             body:  payload.notification.body,
             icon:  "/static/icons/icon-192.png",
-            badge: "/static/icons/icon-192.png"
+            badge: "/static/icons/icon-192.png",
+            vibrate: [200, 100, 200]
         }
     );
 });
